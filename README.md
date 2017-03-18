662185dd94c43a0a4944631ac3f2bdc264e790b4
matzTada <matz@west.sd.keio.ac.jp>
2017-03-18 18:07:05 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,47 @@
+#UltraSonic
+some exlanations...  
+
+
+###Pin assign
+
+Arduino|Other  
+--- | ---   
+0 (RX) |   
+1 (TX) |   
+2 | LCD D4    
+3 | LCD D5   
+4 | LCD D6   
+5 | LCD D7   
+6 |  
+7 | Speaker +  
+8 | Ultrasonic Trig(er)    
+9 | Ultrasonic Echo   
+10 |  
+11 | LCD E (Enable)  
+12 | LCD RW (Read Write)  
+13 |  
+A0 (14) | 
+A1 (15) | 
+A2 (16) | 
+A3 (17) | 
+A4 (18) | 
+A5 (19) | 
+
+
+<a href="http://www.youtube.com/watch?feature=player_embedded&v=YOUTUBE_MOVIE_ID_HERE
+" target="_blank"><img src="http://img.youtube.com/vi/YOUTUBE_MOVIE_ID_HERE/0.jpg " 
+alt="movie on youtube" width=40% border="10" /></a>  
+[movie on youtube](https://www.youtube.com/watch?v=YOUTUBE_MOVIE_ID_HERE)  
+you must change 3 places "YOUTUBE_MOVIE_ID_HERE" to your same movie id.
+
+Overview of this project.
+
+## Proparation
+Explanation.  
+
+* <https://matztada.github.io>  
+* [link](https://matztada.github.io)
+
+## Images
+
+<a><img src="https://matztada.github.io/images/logo.jpg" alt="" width=50%></a>  
---
+++ sketch_mar02dLCDUS/sketch_mar02dLCDUS.ino
@@ -0,0 +1,53 @@
+#include<LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+int Trig = 8;
+int Echo = 9;
+int Duration;
+float Distance;
+
+#define BUZZER_PIN 7
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(Trig, OUTPUT);
+  pinMode(Echo, INPUT);
+  lcd.begin(16, 2);
+
+  pinMode(BUZZER_PIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  float tempDistance = getDistance();
+  lcd.setCursor(0, 0);
+  lcd.print(tempDistance);
+  lcd.setCursor(0, 1);
+  lcd.print(millis() / 1000);
+
+  if (tempDistance > 0) {
+    tone(BUZZER_PIN, (int)map(tempDistance, 0, 150, 200, 800));
+  }
+}
+
+float getDistance() {
+  digitalWrite(Trig, LOW);
+  delayMicroseconds(1);
+  digitalWrite(Trig, HIGH);
+  delayMicroseconds(11);
+  digitalWrite(Trig, LOW);
+  Duration = pulseIn(Echo, HIGH);
+  if (Duration > 0) {
+    Distance = Duration / 2;
+    Distance = Distance * 340 * 100 / 1000000; // ultrasonic speed is 340m/s = 34000cm/s = 0.034cm/us
+    Serial.print(Duration);
+    Serial.print(" us ");
+    Serial.print(Distance);
+    Serial.println(" cm");
+    return Distance;
+  }
+  return -1;
+}
+
